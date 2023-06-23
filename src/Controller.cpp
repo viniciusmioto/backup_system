631699cb355965afb66d19e4e5e53735ba5f1491
viniciusmioto <vinemioto@gmail.com>
2023-06-23 20:30:06 -0300

    fead: send file data module

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -1,24 +1,13 @@
 #include "Communication.hpp"
 
-void sendOneFile(int socket) {
+void sendFileData(int socket, string fileName, int &msgCounter) {
     unsigned char buffer[MAX_SIZE] = {0};
-    int msgCounter = 0;
-    int filePosition = 0;
     int fileSize = 0;
+    int filePosition = 0;
     int bytesRead = 0;
-    string fileName;
     string fileContent;
 
-    cout << "\033[1;36mfile name > \033[0m";
-    cin >> fileName;
-
     fileSize = get_file_size(fileName);
-
-    // send first message to inform the file name - msgCounter = 0
-    Message fileNameMsg(sizeof(fileName), msgCounter, FILE_NAME, (unsigned char *)fileName.c_str(), 0);
-    sendMessage(socket, fileNameMsg);
-    guaranteeSend(socket, fileNameMsg, msgCounter);
-    msgCounter++;
 
     // loop until get all content (limit of MAX_DATA_SIZE bytes each message)
     while (fileSize > 0) {
@@ -31,6 +20,22 @@ void sendOneFile(int socket) {
         guaranteeSend(socket, packageMsg, msgCounter);
         msgCounter++;
     }
+}
+
+void sendOneFile(int socket) {
+    int msgCounter = 0;
+    string fileName;
+
+    cout << "\033[1;36mfile name > \033[0m";
+    cin >> fileName;
+
+    // send first message to inform the file name - msgCounter = 0
+    Message fileNameMsg(sizeof(fileName), msgCounter, FILE_NAME, (unsigned char *)fileName.c_str(), 0);
+    sendMessage(socket, fileNameMsg);
+    guaranteeSend(socket, fileNameMsg, msgCounter);
+    msgCounter++;
+
+    sendFileData(socket, fileName, msgCounter);
 
     adjustMsgCounter(&msgCounter);
 
--- src/Controller.hpp
+++ src/Controller.hpp
@@ -4,6 +4,14 @@
 #include "Communication.hpp"
 
 /*!
+ * \brief Sends the file data to the socket
+ * \param socket
+ * \param fileName: name of the file to be sent
+ * \param msgCounter: sequence number of the message
+ */
+void sendFileData(int socket, string fileName, int &msgCounter);
+
+/*!
  * \brief Sends one file to the socket
  * \param socket
  */
